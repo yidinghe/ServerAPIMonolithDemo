5e9cf985e85da16ae4b5897fed1725c442560910
Yiding He <yhe@truxnow.com>
2020-01-02 16:06:27 -0500

    Added json result wrapper.

--- foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/PassportController.java
+++ foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/PassportController.java
@@ -1,6 +1,7 @@
 package com.imooc.controller;
 
 import com.imooc.service.UserService;
+import com.imooc.utils.IMOOCJSONResult;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,17 +17,17 @@ public class PassportController {
     private UserService userService;
 
     @GetMapping("/usernameIsExist")
-    public int hello(@RequestParam String username) {
+    public IMOOCJSONResult hello(@RequestParam String username) {
 
         if (StringUtils.isBlank(username)) {
-            return 500;
+            return IMOOCJSONResult.errorMsg("用户名不能为空");
         }
 
         boolean isExist = userService.queryUsernameIsExist(username);
         if (isExist) {
-            return 500;
+            return IMOOCJSONResult.errorMsg("用户名已经存在");
         } else {
-            return 200;
+            return IMOOCJSONResult.ok();
         }
     }
 }
