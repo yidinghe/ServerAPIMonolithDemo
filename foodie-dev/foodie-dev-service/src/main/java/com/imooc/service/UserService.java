8178f0547899897b57e1d65e5d1e76f464222385
Yiding He <yhe@truxnow.com>
2020-01-02 16:28:56 -0500

    Added org id generator.

--- foodie-dev/foodie-dev-service/src/main/java/com/imooc/service/UserService.java
+++ foodie-dev/foodie-dev-service/src/main/java/com/imooc/service/UserService.java
@@ -1,6 +1,11 @@
 package com.imooc.service;
 
+import com.imooc.pojo.Users;
+import com.imooc.pojo.bo.UserBO;
+
 public interface UserService {
 
     public boolean queryUsernameIsExist(String username);
+
+    public Users createUser(UserBO userBO);
 }
--- foodie-dev/foodie-dev-service/src/main/java/com/imooc/service/UserServiceImpl.java
+++ foodie-dev/foodie-dev-service/src/main/java/com/imooc/service/UserServiceImpl.java
@@ -1,18 +1,28 @@
 package com.imooc.service;
 
+import com.imooc.enums.Sex;
 import com.imooc.mapper.UsersMapper;
 import com.imooc.pojo.Users;
+import com.imooc.pojo.bo.UserBO;
+import com.imooc.utils.DateUtil;
+import com.imooc.utils.MD5Utils;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
 
+import java.util.Date;
+
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     public UsersMapper usersMapper;
+
+    private static final String USER_FACE = "http://122.152.205.72:88/group1/M00/00/05/CpoxxFw_8_qAIlFXAAAcIhVPdSg994.png";
+
 
     @Transactional(propagation = Propagation.SUPPORTS)
     @Override
@@ -26,5 +36,26 @@ public class UserServiceImpl implements UserService {
         Users result = usersMapper.selectOneByExample(userExample);
 
         return result != null;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public Users createUser(UserBO userBO) {
+
+        Users user = new Users();
+        user.setUsername(userBO.getUsername());
+        try {
+            user.setPassword(MD5Utils.getMD5Str(userBO.getPassword()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        user.setNickname(userBO.getUsername());
+        user.setFace(USER_FACE);
+        user.setBirthday(DateUtil.stringToDate("1900-01-01"));
+        user.setSex(Sex.secret.type);
+        user.setCreatedTime(new Date());
+        user.setUpdatedTime(new Date());
+
+        return user;
     }
 }
